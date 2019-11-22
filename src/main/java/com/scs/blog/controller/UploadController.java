3c8b131a34844e8a606f24b6ca7ff97ebb1aac55
Scui7872 <1610265352@qq.com>
2019-11-22 09:04:30 +0800

    登录/文章/注册/验证码/多文件上传

--- src/main/java/com/scs/blog/controller/UploadController.java
+++ src/main/java/com/scs/blog/controller/UploadController.java
@@ -7,23 +7,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.UUID;
 
-@MultipartConfig(maxFileSize = 1024 * 1024 * 50)
+
+@MultipartConfig
 @WebServlet(urlPatterns = "/api/upload")
 public class UploadController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Part part = req.getPart("filename");
-        String name = part.getSubmittedFileName();
-        System.out.println(name);
-        String path = req.getSession().getServletContext().getRealPath("");
-        System.out.println(path);
-        part.write(path + name);
-        System.out.println(path + name);
-        resp.setContentType("image/jpg");
-        req.setAttribute("msg","上传成功！");
-        req.setAttribute("url","https://localhost:8080/" + name);
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        Collection<Part> parts = req.getParts();
+        long maxFilesize = 1024 * 1024 * 8;
+        for(Part part :parts) {
+            long filesize = part.getSize();
+            System.out.println(filesize);
+            if(filesize>maxFilesize) {
+                req.setAttribute("msg","文件太大，上传失败");
+            } else if(filesize ==0) {
+                req.setAttribute("msg","可文件上传");
+            } else {
+                String name = part.getSubmittedFileName();
+                System.out.println(name);
+                int index = name.indexOf(".");
+                String suffix = name.substring(index);
+                String filename = UUID.randomUUID().toString().replace("-","") + suffix;
+                String path = req.getSession().getServletContext().getRealPath("") + date;
+                System.out.println(path);
+
+                File file = new File(path);
+                if (!file.exists()) {
+                    file.mkdir();
+                }
+                part.write(file.getPath() + "/" + filename);
+                resp.setContentType("image/jpg");
+                req.setAttribute("msg","上传成功！");
+                req.setAttribute("url","https://localhost:8080/" + date + "/" + filename);
+                System.out.println(file.getName());
+                System.out.println(file.getAbsolutePath() + "/" + filename);
+            }
+        }
+//        Part part = req.getPart("filename");
+//        String name = part.getSubmittedFileName();
+//        System.out.println(name);
+//        String path = req.getSession().getServletContext().getRealPath("");
+//        System.out.println(path);
+
+//        part.write(path + name);
+//        System.out.println(path + name);
+
         req.getRequestDispatcher("/upload.jsp").forward(req,resp);
     }
 }
--- src/main/webapp/upload.jsp
+++ src/main/webapp/upload.jsp
@@ -8,13 +8,35 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html lang="en">
 <head>
+    <style type="text/css">
+        .button {
+            width: 150px;
+            height:50px ;
+            /*margin-top: 50px;*/
+            color: aliceblue;
+            border-radius: 5px;
+            background-color: darkcyan;
+            border:1px solid cadetblue;
+
+        }
+        .file {
+            position: fixed;
+            opacity: 0;
+        }
+        .-p {
+            font-size: 14px;
+            color: darkgrey;
+        }
+    </style>
     <meta charset="UTF-8">
     <title>图片上传页面</title>
 </head>
 <body>
 <form action="/api/upload" method="post" enctype="multipart/form-data">
-<input type="file" name="filename">
-<input type="submit" value="上传">
+<input type="file" name="filename" multiple="multiple" class="file">
+<input type="submit" value="上传文件" class="button">
+    <p class="-p">点击“上传文件”上方选择文件</p>
+    <p class="-p">点击“上传文件”下方上传文件</p>
 </form>
 <p>${msg}</p>
 <p>${url}</p>
